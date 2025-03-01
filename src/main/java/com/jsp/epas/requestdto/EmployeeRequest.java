fb5c98be58e31e07a2bc556bbd3d9398a83a0092
Deepakg-code <deepakadhi0808@gmail.com>
2025-03-01 16:54:52 +0530

    Creating employee request dto and resposne dto class for convient request and resposnes

---
+++ src/main/java/com/jsp/epas/requestdto/EmployeeRequest.java
@@ -0,0 +1,16 @@
+package com.jsp.epas.requestdto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeRequest {
+
+    private String employeeName;
+    private String rating;
+}
